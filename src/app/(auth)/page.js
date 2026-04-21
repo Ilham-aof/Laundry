4d1ccc950f4272a2316211f23967e1aeba167e68
ilham-aof <ilham.rogue@gmail.com>
2026-04-21 20:56:14 +0700

    Fix build errors and prepare production deploy

--- src/app/(auth)/page.js
+++ src/app/(auth)/page.js
@@ -1,7 +1,25 @@
 import LoginForm from "./loginForm";
 import { checkAdminExists } from "./action/checkAdmin";
+import { cookies } from "next/headers";
+import { prisma } from "../../utils/prisma";
+import { redirect } from "next/navigation";
 
-export default async function loginPage() {
+export default async function LoginPage() {
+  const cookieStore = await cookies();
+  const sessionId = cookieStore.get("sessionId")?.value;
+
+  if (sessionId) {
+    const session = await prisma.session.findUnique({
+      where: {
+        id: sessionId,
+      },
+    });
+
+    if (session) {
+      redirect("/dashboard");
+    }
+  }
+
   const isAdminExists = await checkAdminExists();
   return (
     <>
